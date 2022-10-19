dc9d96c2ac8290086e690ee4f87a58247ef46eee
Andy W <andriejw@gmail.com>
2022-10-19 21:11:45 +0200

    first with fifth

---
+++ main.c
@@ -0,0 +1,12 @@
+
+
+#include <string>
+
+int main(void){
+  cout << "First LED" << endl;
+  cout << "Second LED" << endl;
+  cout << "Third LED" << endl;
+  cout << "Fourth LED" << endl;
+  cout << "Fifth LED" << endl;
+  cout << "Sixth LED" << endl;
+}
